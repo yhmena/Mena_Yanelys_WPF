c678661050606e92a952b6821f9d5c262236a101
Yanelys Mena <yhmena@fullsail.edu>
2013-10-06 21:15:10 -0400

    These are a few files that I worked on while watching the screencasts. I am so excited and can't believe I learned so much just by watching a few videos!!

---
+++ Values and Variables Screencasts/Array.js
@@ -0,0 +1,12 @@
+// Arrays
+
+var muppetNames = ["Kermit", "MissPiggy", "Gonzo", "Rat"];
+//* console.log(muppetNames); *//
+
+//* console.log(muppetNames[0]); *//
+
+//* console.log(muppetNames[1]); *//lets me see whats in index #1
+
+//* console.log(
+
+
