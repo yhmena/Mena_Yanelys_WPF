f9629b37e40880b019efa0e3ba648c9669f591a6
Yanelys Mena <yhmena@fullsail.edu>
2013-10-09 14:17:38 -0400

    Arithmetic Operators checking to see if formula works

---
+++ Screencast Expressions/Arithmetic Operators/js/script.js
@@ -0,0 +1,11 @@
+// Expressions - Artihmetic Operators
+
+//area of the triangle is half of the width times the height
+var width = 4;
+var height = 5;
+var area;
+
+area = width * height;
+
+console.log(area);
+
