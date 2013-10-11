04049b9c35b20942631a66a74783f4d164b7ef26
Yanelys Mena <yhmena@fullsail.edu>
2013-10-10 22:57:08 -0400

    Expressions worksheet

--- Expressions_worksheet/Average shopping bill/js/script.js
+++ Expressions_worksheet/Average shopping bill/js/script.js
@@ -2,15 +2,19 @@
 // 10-09-2013
 // Expressions Worksheet
 
-//Calculate Monthly Travel Expenses
+//Calculate my average shopping bill
 
-//Declaring Variables and Arrays
-var MilesPerDay = prompt("Number of Roundtrip miles per commite day:");
-var CommuteDays = prompt("Number of commute days per month:");
-var GasPerGallon = prompt("Cost of gas per gallon:");
-var MilesPerGallon = prompt("Number of Car Miles Per Gallon:");
-var Extracosts = prompt([1,2,3]);
+//Storing my 5 grocery totals in an array
+var week = [40, 60, 30, 20, 55];
 
+//Calculating total costs
+var total = week[0] + week[1] + week[2] + week[3] + week[4];
+
+//Creating an expression to calculate the average
+var average = total / 5;
+
+//Printing out the total and average of my shopping bills
+console.log("You have spent a total of $" + total + " on groceries over 5 weeks. This is an average of $" + average + " per week.");
 
 
  
