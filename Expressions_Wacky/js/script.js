08777861abe330905b51ba94ea9a321a38c78943
Yanelys Mena <yhmena@fullsail.edu>
2013-10-10 23:33:00 -0400

    Expressions Wacky Halfway through

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -7,4 +7,8 @@ alert("Hello There!");
 
 var MonthlyIncome = prompt("What is your monthly income?");
 var Rent = prompt("How much do you pay in rent?");
-var Expenses = prompt("What is the total cost of other expenses?");
+var Expenses = prompt("What is the estimated total cost of other expenses per month?");
+
+//Calculating remaining funds
+var Total = Rent + Expenses;
+var Leftover = MonthlyIncome - Total;
