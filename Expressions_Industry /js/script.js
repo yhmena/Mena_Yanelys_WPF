207fdd5b4ba1d8f7d459fb222b34a9214ffd2403
Yanelys Mena <yhmena@fullsail.edu>
2013-10-10 22:17:53 -0400

    industry Expressions files

--- Expressions_Industry /js/script.js
+++ Expressions_Industry /js/script.js
@@ -1,14 +1,14 @@
 // Yanelys Mena
 // 10-10-2013
-// Expressions: Industry - 
+// Expressions: Industry - Designing a Poster for Work
 
 //My main function at work is to support a Production Application. My boss has given me a project to work on. I must create a poster describing the 5 step Process that takes place. I need to fit all the information in a limited poster space. I also need to frame the poster, which decreases the poster space for me to design on. 
 
-//Declaring Variables
+//Declaring and Assigning Variables
 var posterwidth = prompt("What is the width of your poster?");
 var posterheight = prompt("What is the height of your poster?");
 var framewidth = prompt("What is the width of the inner part of the frame?");
-var frameheight = prompt("What is the height of the innter part of the frame?");
+var frameheight = prompt("What is the height of the inner part of the frame?");
 
 //Calculatating Space to draw on:
 var PosterArea = posterwidth * posterheight;
