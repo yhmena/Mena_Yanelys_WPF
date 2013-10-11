106fe0cdf005b0ec9267585afd184142447b046e
Yanelys Mena <yhmena@fullsail.edu>
2013-10-10 22:06:06 -0400

    Adding more details to Industry Expressions Assignment.

--- Expressions_Industry /js/script.js
+++ Expressions_Industry /js/script.js
@@ -2,12 +2,12 @@
 // 10-10-2013
 // Expressions: Industry - 
 
-//Calculate how many assignments to balance
+//My main function at work is to support a Production Application. My boss has given me a project to work on. I must create a poster describing the 5 step Process that takes place. I need to fit all the information in a limited poster space. I also need to frame the poster, which decreases the poster space for me to design on. 
 
-//Balancing Average In my current job there are 
+//Declaring Variables
 var posterwidth = prompt("What is the width of your poster?");
 var posterheight = prompt("What is the height of your poster?");
-var frame = prompt("Will you frame your poster?");
+var frame = alert("Will you frame your poster?");
 var framealert = alert("Framing your poster will reduce the amount of space to draw on. Press OK if you would like to continue.");
 var framewidth = prompt("What is the width of the Inner part of the frame?");
 var frameheight = prompt("What is the height of the innter part of the frame?");
@@ -17,6 +17,8 @@ var PosterArea = posterwidth * posterheight;
 var FrameArea = framewidth * frameheight;
 var PosterSpaceNotUsed = PosterArea - FrameArea; 
 var PosterAreaLeft = PosterArea - PosterSpaceNotUsed;
+
+console.log();
 
 
 
