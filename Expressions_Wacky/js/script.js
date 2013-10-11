effd9df01f466e3a4d375a0cc1272f033ffda8e6
Yanelys Mena <yhmena@fullsail.edu>
2013-10-10 23:29:03 -0400

    Expressions Wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,6 +2,9 @@
 // 10-09-2013
 // Expressions Wacky
 
-//
 
- 
+alert("Hello There!");
+
+var MonthlyIncome = prompt("What is your monthly income?");
+var Rent = prompt("How much do you pay in rent?");
+var Expenses = prompt("What is the total cost of other expenses?");
