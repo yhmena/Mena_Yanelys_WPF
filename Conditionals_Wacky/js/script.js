43c2bb47129669ed86173a21c51354a1dc4e645a
Yanelys Mena <yhmena@fullsail.edu>
2013-10-17 19:48:11 -0400

    no message

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,28 @@
+//Ternaary Operators
+
+// var gpa = 48;
+
+//if gpa is over the min 2.0 score, the student can graduate
+/* if(gpa > 2.0){
+	console.log("You can graduate!");
+	
+}else{
+	console.log("GPA is too low!");
+} */
+
+//*(gpa > 2.0) ? console.log("You can Graduate!") : console.log("Gpa too low!"); *//
+
+var age = 11;
+var book;
+
+// if(age <10){
+//	book = "Green Eggs and Ham"
+//}else{
+//	book = "Time Machine";
+//}
+//console.log(book); 
+
+book = (age < 10) ? "Green Eggas and Ham" : "The Time Machine";
+console.log(book);
+
+
