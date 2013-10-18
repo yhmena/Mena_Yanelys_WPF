9b7646520df2064f796bce143fd95324aa3372f3
Yanelys Mena <yhmena@fullsail.edu>
2013-10-17 23:08:19 -0400

    Setting up variables and assigning values, also converting strings to Integers

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,3 +4,8 @@
 
 // Schedule Aid: How much time left to actually do your school homework?
 
+var string1 = prompt("How many hours do you work daily?");
+var workHours = (string1);
+var string2 = prompt("How many hours does it take to commute to and from work daily?");
+var commute = (string2);
+var string3 = prompt("Enter the amount of hours of sleep you need to funtion:");
