34db2518c5f27e5b7489a7a72154d86a95a4207d
Yanelys Mena <yhmena@fullsail.edu>
2013-10-17 23:48:13 -0400

    Final Commit for Industry Conditionals Assignment!

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,11 +3,11 @@
 //Conditionals: Industry
 
 // Schedule Aid: How many hours a week do you have left over to do howework?
-//This is based on Full Sails curriculum of hours
-//Example: Use WPF Week 4 total estimated HW time of 2 hours
+//This is based on Full Sail's curriculum of hours per week.
+//For Example: Next Week in this class there is a total estimated HW time of 2 hours
 //Lets calculate if you have enough time leftover to do hw
 
-alert("Welcome to Homework Aid! We can help you figure out how many hours you have left throughout the week to actually do your assignments!");
+alert("Welcome to the HW Time Management Calculator! We can help you figure out how many hours you have left throughout the week to actually do your assignments!");
 
 var string1 = prompt("Enter the number of Assignment Hours for one Full Sail Course Week:");
 FSUHoursWeekly = parseInt(string1);
@@ -23,12 +23,9 @@ var HoursBusyWeek = 7 * (workHours + commute + sleep)
 
 var HoursLeftWeek = (24 * 7 - HoursBusyWeek);
 
-(HoursLeftWeek > FSUHoursWeekly) ? console.log("You have " + HoursLeftWeek + " hours per week for Full Sail Assignments.") : console.log("You don't have any time left during your week for your homework. Make Time!");
+//Ternary Conditional Expression Shortcut
+//format is (conditional) ? do this if true : do this if false;
+(HoursLeftWeek > FSUHoursWeekly) ? console.log("You have " + HoursLeftWeek + " hours per week for Full Sail Assignments.") + alert("You have " + HoursLeftWeek + " hours per week for Full Sail Assignments.") : console.log("You don't have any time left during your week for your homework. Make Time!") + alert("You don't have any time left during your week for your homework. Make Time!");
 
-//Calculate Total Hours taken up by work, commute and sleep
 
-//Numbers used for conditional expressions are: 24 hours per day and 7 days a week
-
-//Does your spare time exceed the left over than your assigned HW time?
-//(FSUHours > totalHours) ? console.log("You do not have time for homework") : console.log("You do have time for homework");
 
