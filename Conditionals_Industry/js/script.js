06f47df4b3e5409331d4ebac7cb240510a6954ed
Yanelys Mena <yhmena@fullsail.edu>
2013-10-17 23:04:22 -0400

    Setting the topic for Industry Assignment

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,28 +1,6 @@
-//Ternaary Operators
+//Yanelys Mena
+//October 17, 2013
+//Conditionals: Industry
 
-// var gpa = 48;
-
-//if gpa is over the min 2.0 score, the student can graduate
-/* if(gpa > 2.0){
-	console.log("You can graduate!");
-	
-}else{
-	console.log("GPA is too low!");
-} */
-
-//*(gpa > 2.0) ? console.log("You can Graduate!") : console.log("Gpa too low!"); *//
-
-var age = 11;
-var book;
-
-// if(age <10){
-//	book = "Green Eggs and Ham"
-//}else{
-//	book = "Time Machine";
-//}
-//console.log(book); 
-
-book = (age < 10) ? "Green Eggas and Ham" : "The Time Machine";
-console.log(book);
-
+// Schedule Aid: How much time left to actually do your school homework?
 
