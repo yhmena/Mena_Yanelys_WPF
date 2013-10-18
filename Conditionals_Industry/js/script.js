6987efd5405157c2d8c6cf9ee35e19ed1582b771
Yanelys Mena <yhmena@fullsail.edu>
2013-10-17 23:15:04 -0400

    Attempting to create an issue to solve or form a calculation of a sort

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,10 +2,22 @@
 //October 17, 2013
 //Conditionals: Industry
 
-// Schedule Aid: How much time left to actually do your school homework?
+// Schedule Aid: How many hours a week do you have left over to do howework?
+//This is based on Full Sails curriculum of hours
+//Example: Use WPF Week 4 total estimated HW time of 2 hours
+//Lets calculate if you have enough time leftover to do hw
+
+alert("Welcome to Homework Aid! We can help you figure out how many hours you have left throughout the week to actually do your assignments!");
+
 
 var string1 = prompt("How many hours do you work daily?");
 var workHours = (string1);
 var string2 = prompt("How many hours does it take to commute to and from work daily?");
 var commute = (string2);
 var string3 = prompt("Enter the amount of hours of sleep you need to funtion:");
+var sleep = (string3);
+
+
+
+//Numbers used for conditional expressions are: 24 hours per day and 7 days a week
+
