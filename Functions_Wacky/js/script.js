6aaedd2ac15148dcb26c806e7f327efe3906770c
Yanelys Mena <yhmena@fullsail.edu>
2013-10-24 23:50:20 -0400

    Setting up function, assigning variables, making calculations

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -7,3 +7,8 @@
 //Calculate the average amount of cupcakes needed for each classroom 
 
 Calculate(20, 19, 16); //This represents the number of students in 3 seperate classrooms
+function Calculate(n1, n2, n3) { 
+  var average = (n1 +  n2 + n3) / 3 //formula to calculate the average cupcakes per class
+ console.log(average); 
+}
+
