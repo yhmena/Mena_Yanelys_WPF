cf625793e59ea34944075f63a3a80649ac1de7db
Yanelys Mena <yhmena@fullsail.edu>
2013-10-24 23:38:40 -0400

    Informing the user what is being calculated

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,3 +6,5 @@
 //Determine Calories burned while jogging
 //Formula: .75 * Weight in Pounds * # of miles ran
 
+alert("Let's calculate the number of calories you'll burn while running!"); //Informing the user what is being calculated
+
