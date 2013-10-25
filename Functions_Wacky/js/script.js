16fcce4c6b8221726a4e1402c31a9d530f29389d
Yanelys Mena <yhmena@fullsail.edu>
2013-10-24 23:56:57 -0400

    Added a finishing touch to the console.log print out

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -9,7 +9,7 @@
 Calculate(20, 19, 16); //This represents the number of students in 3 seperate classrooms
 function Calculate(n1, n2, n3) { //Here I am setting the parameters that will go along with the arguments above
  var average = (n1 +  n2 + n3) / 3; 
- console.log(average); 
+ console.log("The average cupcakes you will need per classroom is "+ average + "."); 
  
 //The variable "average" calcualtes the average amount of students per class, hence, the average amount of 
 //cupcakes I should bring to school
