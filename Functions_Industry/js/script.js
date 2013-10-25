0bb5de9aa64748001c9915d1dd7dbb774b114339
Yanelys Mena <yhmena@fullsail.edu>
2013-10-24 23:43:36 -0400

    Declaring Variables and assigning values (prompt) and user input

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,3 +4,6 @@
 
 //Can my department afford to splurge on a new conference room table?
 
+var expenses = prompt("Enter your departments staple monthly expenses:"); //User inputs monthly expenses
+var budget = prompt("What is your departments monthly budget?"); //user inputs monthly budget 
+var priceofTV = prompt("Enter the cost of the item:"); //user inputs cost of item
