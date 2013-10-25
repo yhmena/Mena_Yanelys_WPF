5e6bd9b1896fcdcfd8152ed257ee678819766077
Yanelys Mena <yhmena@fullsail.edu>
2013-10-24 23:39:24 -0400

    Grabing information input by user

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -7,4 +7,5 @@
 //Formula: .75 * Weight in Pounds * # of miles ran
 
 alert("Let's calculate the number of calories you'll burn while running!"); //Informing the user what is being calculated
+var expected = prompt("How many calories are you expecting to burn?") //Grabing information input by user
 
