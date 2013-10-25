80c280136773eba467fa903f8c2d5a66ff3af0b7
Yanelys Mena <yhmena@fullsail.edu>
2013-10-24 20:15:10 -0400

    Calculating the circumference of a circle Commit #1
    Establishing function, parameters, and arguments

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,14 @@
+//Yanelys Mena
+//Functions Expressions
+//October 24, 2013
+
+
+
+//
+var total = circumference(30, 20);
+
+function circumference(radius){
+	var area = w * h;
+	return circumference;
+}
+console.log("The circumference of the circle is " + total + ".");
