d15874c9216851b64cb4ac751f0d70e5e0a09069
Yanelys Mena <yhmena@fullsail.edu>
2013-10-24 20:24:47 -0400

    Establishing Variable so that return can work

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -6,7 +6,7 @@
 //Calculate the circumference of a circle
 //FORMULA >> C = 2 * Pie * radius
 
-var total = Calc(5);
+var total = Calc(5); //Establishing variable so for Return
 
 function Calc(radius){
 	var circumference = 2 * 3.14 * radius;
