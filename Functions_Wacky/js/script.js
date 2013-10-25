31de73d100f82cf44ba7a58ffc0d95b7bd1c9dea
Yanelys Mena <yhmena@fullsail.edu>
2013-10-24 23:53:04 -0400

    Adding some notes to function code

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -7,8 +7,12 @@
 //Calculate the average amount of cupcakes needed for each classroom 
 
 Calculate(20, 19, 16); //This represents the number of students in 3 seperate classrooms
-function Calculate(n1, n2, n3) { 
-  var average = (n1 +  n2 + n3) / 3 //formula to calculate the average cupcakes per class
+function Calculate(n1, n2, n3) { //Here I am setting the parameters that will go along with the arguments above
+ var average = (n1 +  n2 + n3) / 3; 
  console.log(average); 
-}
+ 
+//The variable "average" calcualtes the average amount of students per class, hence, the average amount of 
+//cupcakes I should bring to school
+//console. log prints out results for developer to view
 
+}
